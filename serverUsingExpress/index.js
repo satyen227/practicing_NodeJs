5789563bbd7f76c93fc18779e0b7498fcebf3f99
Satyen0592 <satyen212@gmail.com>
2025-01-08 08:49:06 +0530

    Routing

--- serverUsingExpress/index.js
+++ serverUsingExpress/index.js
@@ -6,7 +6,25 @@ app.listen(port, () => {
   console.log(`App is listening at port ${port}`);
 });
 
-app.use((req, res) => {
-  //console.log("Request Recived");
-  res.send("Hey there how are you ,i am killua thanks for connecting");
+// app.use((req, res) => {
+//   //console.log("Request Recived");
+//   let code = "<h1>Friuts</h1> <ul><li>Apple</li><li>Mango</li></ul>";
+//   //   res.send("Hey there how are you ,i am killua thanks for connecting");
+//   res.send(code);
+// });
+
+app.get("/", (req, res) => {
+  res.send("You are seeing main page");
+});
+app.get("/help", (req, res) => {
+  res.send("You are seeing Help Page , HOW CAN I HELP YOU !!");
+});
+app.get("/search", (req, res) => {
+  res.send("you are seeing search page");
+});
+app.get("*", (req, res) => {
+  res.send("The page you are looking for does not exist");
+});
+app.post("/", (req, res) => {
+  res.send("sending post request");
 });
