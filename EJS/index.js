e1b4435334cc4863ced44ac8e2c136359eda00fb
Satyen0592 <satyen212@gmail.com>
2025-01-10 09:07:14 +0530

    defineing path in ejs

--- EJS/index.js
+++ EJS/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const path = require("path");
 
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  res.render("insta.ejs");
 });
 
 app.listen(port, () => {
--- serverUsingExpress/index.js
+++ serverUsingExpress/index.js
@@ -6,13 +6,25 @@ app.listen(port, () => {
   console.log(`App is listening at port ${port}`);
 });
 
-// app.use((req, res) => {
-//   //console.log("Request Recived");
-//   let code = "<h1>Friuts</h1> <ul><li>Apple</li><li>Mango</li></ul>";
-//   //   res.send("Hey there how are you ,i am killua thanks for connecting");
-//   res.send(code);
-// });
+//path parameters
 
+app.get("/:username/:id", (req, res) => {
+  let { username, id } = req.params;
+  res.send(`Welcome @${username} your id is ${id}`);
+});
+
+/*
+// response
+
+
+app.use((req, res) => {
+  //console.log("Request Recived");
+  let code = "<h1>Friuts</h1> <ul><li>Apple</li><li>Mango</li></ul>";
+  //   res.send("Hey there how are you ,i am killua thanks for connecting");
+  res.send(code);
+});
+
+// Routing 
 app.get("/", (req, res) => {
   res.send("You are seeing main page");
 });
@@ -22,9 +34,13 @@ app.get("/help", (req, res) => {
 app.get("/search", (req, res) => {
   res.send("you are seeing search page");
 });
+app.get("/shop", (req, res) => {
+  res.send("Welcome to Shop Page , What do You like to Buy");
+});
 app.get("*", (req, res) => {
   res.send("The page you are looking for does not exist");
 });
 app.post("/", (req, res) => {
   res.send("sending post request");
 });
+*/
