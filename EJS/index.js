d14f32b242ecbd9115e29bfd445a9e5807ce4c06
Satyen0592 <satyen212@gmail.com>
2025-01-10 20:59:13 +0530

    .....

--- EJS/index.js
+++ EJS/index.js
@@ -6,8 +6,13 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
-app.get("/", (req, res) => {
-  res.render("insta.ejs");
+app.get("/insta/:username", (req, res) => {
+  let { username } = req.params;
+  res.render("insta.ejs", { username });
+});
+app.get("/rolldice", (req, res) => {
+  let diceValue = Math.floor(Math.random() * 6) + 1;
+  res.render("rolldice.ejs", { diceValue });
 });
 
 app.listen(port, () => {
